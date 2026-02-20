58ae25eac35b8a0fa98d3a9c0667c48ee72e3903
kathya2006 <lnvkathya2006@gmail.com>
2026-02-20 10:28:38 +0530

    UC3-added Reverse string

--- src/UseCase1PalindromeCheckerApp.java
+++ src/UseCase1PalindromeCheckerApp.java
@@ -1,27 +1,25 @@
 public class UseCase1PalindromeCheckerApp {
+
         public static void main(String[] args) {
 
-            // Hardcoded string (String Literal)
-            String word = "madam";
+            // Original String
+            String original = "level";
 
-            System.out.println("Palindrome Checker App - UC2");
-            System.out.println("Checking word: " + word);
-
-            // Reverse the string
+            // Variable to store reversed string
             String reversed = "";
 
-            for (int i = word.length() - 1; i >= 0; i--) {
-                reversed += word.charAt(i);
+            // Reverse string using for loop
+            for (int i = original.length() - 1; i >= 0; i--) {
+                reversed = reversed + original.charAt(i);
             }
 
-            // Conditional check using if-else
-            if (word.equals(reversed)) {
-                System.out.println("Result: The word is a Palindrome.");
+            // Compare original and reversed string
+            if (original.equals(reversed)) {
+                System.out.println("The string \"" + original + "\" is a Palindrome.");
             } else {
-                System.out.println("Result: The word is NOT a Palindrome.");
+                System.out.println("The string \"" + original + "\" is NOT a Palindrome.");
             }
-
-            System.out.println("Program Ended.");
         }
     }
+
 
