ac4439392bb257d8e365c0e2eebc297093004e1c
kathya2006 <nl2069@srmist.edu.in>
2026-03-16 10:34:53 +0530

    UC1 added

---
+++ BookMyStay/src/HotelBookingApp.java
@@ -0,0 +1,16 @@
+public class HotelBookingApp {
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("==================================");
+        System.out.println(" Welcome to Hotel Booking System ");
+        System.out.println(" Version: 1.0 ");
+        System.out.println("==================================");
+
+        // Inform user that application has started
+        System.out.println("Application started successfully.");
+
+        // Inform user that program execution is complete
+        System.out.println("Application terminated.");
+    }
+}
